1a180955d0b030d29a30f169f79b5430fbc41de3
Swadhin Ghosh <43653792+Swadhincse@users.noreply.github.com>
2023-04-02 17:32:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-//your code here
+const prices = Array.from(document.querySelectorAll('[data-ns-test="prices"]'))
+  .map(priceElem => parseFloat(priceElem.innerText));
+
+const totalPrice = prices.reduce((acc, price) => acc + price, 0);
+
+const table = document.querySelector('table');
+const newRow = table.insertRow();
+const newCell = newRow.insertCell();
+newCell.colSpan = 2;
+newCell.innerText = `Total Price: $${totalPrice.toFixed(2)}`;
+
+newRow.setAttribute('data-ns-test', 'grandTotal');
+newCell.setAttribute('data-ns-test', 'grandTotal');
